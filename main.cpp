f7fb4716de213e0518ee955d907b3cc312008ccb
RitaRez <ritarezborges@gmail.com>
2019-09-08 11:28:59 -0300

    read file function working

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+std::vector<std::string> read_file(std::string fileName){
+    
+    std::string object;
+    std::vector<std::string> objectsArray;
+    std::ifstream inFile;
+
+
+    inFile.open(fileName);
+    if (!inFile) {
+        std::cerr << "Unable to open file";
+        exit(2); 
+    } 
+
+    while(!inFile.eof()){
+        getline(inFile, object);
+        objectsArray.push_back(object);
+    }
+    inFile.close();
+
+    return objectsArray;
+}
+
+
+int main(int argc, const char** argv) {
+    
+    std::ifstream inFile;
+    std::vector<std::string> UsersArray;
+    std::vector<std::string> EventsArray;
+
+    if(argc != 3) {
+        std::cerr << "There arent enough arguments";
+        exit(1);
+    }
+    
+    UsersArray = read_file(argv[1]);
+    EventsArray = read_file(argv[2]);
+
+
+    return 0;
+}

16f379c6f7b6a43f2fdb68a8aabc5126655d4773
RitaRez <ritarezborges@gmail.com>
2019-09-10 11:05:46 -0300

    Created file parser class

--- main.cpp
+++ main.cpp
@@ -8,80 +8,8 @@
 #include <vector> 
 #include <map> 
 
+#include "./lib/file_parser.hpp"
 
-#include "./lib/users/user.hpp"
-#include "./lib/users/kid.hpp"
-#include "./lib/users/adult.hpp"
-#include "./lib/users/elder.hpp"
-
-#include "./lib/events/event.hpp"
-#include "./lib/events/infant_event.hpp"
-#include "./lib/events/puppet_show.hpp"
-#include "./lib/events/adult_event.hpp"
-#include "./lib/events/club.hpp"
-#include "./lib/events/consert.hpp"
-#include "./lib/events/movie_theater.hpp"
-
-
-
-void parse_users(std::vector<std::string> StringArray, std::map<int, Kid> *kids, std::map<int, Adult> *adults, std::map<int, Elder> *elders){
-    
-    std::vector<std::string> aux;
-    
-    for(std::string str : StringArray){
-        std::stringstream ss(str);
-        std::string token;
-        
-        while (std::getline(ss, token, ',')){ 
-            token = token.c_str();
-            aux.push_back(token);
-        }  
-
-        if(aux[1].compare("crianca") == 0){     
-            Kid newKid(std::stoi(aux[0]), aux[1], aux[2], std::stoi(aux[3]), std::stof(aux[4]), std::stoi(aux[5]));
-            (*kids)[newKid.get_id()] = newKid;
-            (*adults)[std::stoi(aux[5])].add_children(newKid.get_id());
-         
-         } else if(aux[1].compare("adulto") == 0){
-            Adult newAdult(std::stoi(aux[0]),aux[1], aux[2],std::stoi(aux[3]),std::stof(aux[4]));
-            (*adults).insert({newAdult.get_id(), newAdult});    
-        
-        } else {
-            Elder newElder(std::stoi(aux[0]),aux[1], aux[2],std::stoi(aux[3]),std::stof(aux[4]));
-            (*elders)[newElder.get_id()] = newElder;   
-        }
-        aux.clear();
-    } 
-}
-
-void parse_events(std::vector<std::string> StringArray, std::map<int, MovieTheater> *movieTheaters, std::map<int, PuppetShow> *puppetShows, std::map<int, Consert> *conserts, std::map<int, Club> *clubs){
-    std::vector<std::string> aux;
-    
-    for(std::string str : StringArray){
-        std::stringstream ss(str);
-        std::string token;
-        
-        while (std::getline(ss, token, ',')){ 
-            token = token.c_str();
-            aux.push_back(token);
-        }         
-        
-        if(aux[1].compare("infantil") == 0){    
-            // PuppetShow newPuppetShow(std::stoi(aux[0]), aux[2], aux[3], std::stoi(aux[3]), std::stof(aux[4]));
-            // (*puppetShows).insert({newPuppetShow.get_id()});
-
-        } else if(aux[1].compare("adulto") == 0){   
-            if(aux[2].compare("boate") == 0) {
-
-            } else{
-
-            }
-        } else {
-
-        }
-        aux.clear();
-    } 
-}
 
 std::vector<std::string> read_file(std::string fileName){
     
@@ -120,17 +48,9 @@ int main(int argc, const char** argv) {
     usersArray = read_file(argv[1]);
     eventsArray = read_file(argv[2]);
 
-    std::map<int, Kid> *kids = new std::map<int, Kid>;
-    std::map<int, Adult> *adults = new std::map<int, Adult>;
-    std::map<int, Elder> *elders = new std::map<int, Elder>;
-    parse_users(usersArray, kids, adults, elders);
-
-
-    std::map<int, MovieTheater> *movieTheaters = new std::map<int, MovieTheater>;
-    std::map<int, PuppetShow> *puppetShows = new std::map<int, PuppetShow>;
-    std::map<int, Consert> *conserts = new std::map<int, Consert>;
-    std::map<int, Club> *clubs = new std::map<int, Club>;
-    parse_events(eventsArray, movieTheaters, puppetShows, conserts, clubs);
+    FileParser fp(usersArray, eventsArray);
+    fp.parse_users();
+    fp.parse_events();
 
     return 0;
 }
